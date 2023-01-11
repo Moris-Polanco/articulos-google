70f4c6a28214ad5176a9e40ddb9ff70b454a7ece
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-01-11 07:55:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,20 +11,20 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 st.title('GPT-3 Text-Davinci-003')
 
 # Mostrar una imagen
-st.image('https://i.imgur.com/3TfTl8A.png')
+# st.image('https://i.imgur.com/3TfTl8A.png')
 
 # Mostrar una descripción
-st.markdown("Esta aplicación usa GPT-3 Text-Davinci-003 de OpenAI para producir artículos basados en búsquedas en Google.")
+st.markdown("Esta aplicación usa GPT-3 Text-Davinci-003 de OpenAI para producir artículos.")
 
 # Obtener la búsqueda del usuario
-query = st.text_input('Introduzca su búsqueda:')
+query = st.text_input('Introduzca el título de su artículo:')
 
 # Generar el artículo
 if query:
     article = openai.Completion.create(
         engine="text-davinci-003",
         prompt=query,
-        max_tokens=1500,
+        max_tokens=2500,
         temperature=0.7,
         top_p=0.9
     )
@@ -32,4 +32,4 @@ if query:
 
 # Mostrar un botón de créditos
 if st.button('Créditos'):
-    st.markdown('Creado por [David López](https://github.com/davidlopezd) usando [OpenAI](https://openai.com/) y [Streamlit](https://www.streamlit.io/).')
+    st.markdown('Creado por [Moris Polanco] usando [OpenAI](https://openai.com/) y [Streamlit](https://www.streamlit.io/).')
